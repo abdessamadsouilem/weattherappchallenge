3855545b755e52da3f1d7603b6e1855783e8e6d8
= <=>
2024-02-21 10:51:17 +0000

    setup db connection

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = ()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("connection to db establish successfully");
+    }).catch((err)=>{
+        console.log("failed to connect to mongodb");
+    })
+}
+
+module.exports = connection
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const server = express();
+const connection = require("./config/db")
+require("dotenv").config();
+const PORT = process.env.PORT || 8080
+
+
 
 
 server.get("/",(req,res)=>{
@@ -10,8 +15,9 @@ server.get("/",(req,res)=>{
 
 
 
-server.listen(3000,()=>{
+server.listen(PORT,()=>{
     console.log('====================================');
-    console.log("server up and running ");
+    console.log("server up and running at "+PORT);
+    connection();
     console.log('====================================');
 })
