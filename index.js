230658a619aa8e896c55c4398e367c11e69154f8
= <=>
2024-02-23 00:37:37 +0000

    add cors to our app

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express")
 const server = express();
+const cors = require("cors")
 const connection = require("./config/db")
 require("dotenv").config();
 const PORT = process.env.PORT || 8080
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log('Origin attempting to access:', origin);
+        if (process.env.FRONT_URI === origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+server.use(cors(corsOptions));
+
+
 
 
 const weatherRoutes = require('./routes/weatherRoutes');
