f8f8eeea3915c64138914f08de9b36105a5db4a7
= <=>
2024-02-23 18:29:42 +0000

    update error handling

--- services/weatherService.js
+++ services/weatherService.js
@@ -3,9 +3,6 @@ const Weather = require('../models/weatherModel');
 exports.getWeatherByCity = async (city) => {
     try {
         const weatherData = await Weather.findOne({"location.name": city});
-        if (!weatherData) {
-            throw new Error('City not found');
-        }
         return weatherData;
     } catch (error) {
         throw new Error(`Error retrieving weather data: ${error.message}`);
